5fb4cd4fe5d11ceafa65def054ab84be15ce4423
abBug <13782434+abbug@user.noreply.gitee.com>
2024-07-06 00:27:08 +0800

    实现部分页面需要隐藏底部导航栏效果

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import tabBar from './components/tab-bar/tab-bar.vue';
+import { useRoute } from 'vue-router'
+const route = useRoute()
 </script>
   
 <template>
   <router-view></router-view>
-  <tabBar></tabBar>
+  <tabBar v-if="!route.meta.hideTabBar"></tabBar>
 </template>
 
 <style scoped>
---
+++ src/views/home/cpns/home-search-box.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="banner">
+      <img src="@/assets/img/home/banner.webp" alt="">
+    </div>
+    <div class="location">
+      <div class="city" @click="cityClick">广州</div>
+      <div class="position" @click="positionClick">
+        <span class="text">我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter()
+// 位置/城市
+const cityClick = () => {
+  router.push("/city")
+}
+const positionClick = () => {
+  navigator.geolocation.getCurrentPosition(res => {
+    console.log("获取位置成功:", res)
+  }, err => {
+    console.log("获取位置失败:", err)
+  }, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  })
+}
+</script>
+
+<style lang="less" scoped>
+.banner {
+  img {
+    width: 100%;
+  }
+}
+.location {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 20px;
+
+  .city {
+    flex: 1;
+  }
+
+  .position {
+    width: 74px;
+
+    .text {
+      position: relative;
+      top: 1px;
+      font-size: 12px;
+    }
+
+    img {
+      margin-left: 5px;
+      width: 18px;
+      height: 18px;
+    }
+  }
+}
+</style>
