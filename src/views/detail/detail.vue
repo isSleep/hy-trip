8c0c468cbbc35a0b2cfbcb5f84c94399f025166d
abBug <13782434+abbug@user.noreply.gitee.com>
2024-07-10 20:53:44 +0800

    获取房屋详情数据

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="detail">
-    <h2>detail{{route.params.id}}</h2>
+    <van-nav-bar
+      title="房屋详情"
+      left-text="旅途"
+      left-arrow
+      @click-left="onClickLeft"
+    />
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { ref, computed } from 'vue'
+import { useRoute,useRouter } from 'vue-router';
+import { getDetailInfos } from '@/services'
+
+const router = useRouter()
 const route = useRoute()
+const houseId = route.params.id
+
+// 发送网络请求
+const detailInfos = ref({})
+const mainPart = computed(() => detailInfos.value.mainPart)
+getDetailInfos(houseId).then(res => {
+    detailInfos.value = res.data
+})
+
+// 监听返回按钮的点击
+const onClickLeft = () => {
+    router.back()
+}
+
+
 </script>
 
 <style lang="less" scoped>
