b8118694a763bc69d60e5e7440d2fed556f88fda
abBug <13782434+abbug@user.noreply.gitee.com>
2024-07-10 15:22:38 +0800

    detail路由搭建

--- src/views/home/cpns/home-content.vue
+++ src/views/home/cpns/home-content.vue
@@ -3,8 +3,8 @@
         <h2 class="title">热门精选</h2>
         <div class="list">
             <template v-for="(item, index) in houselist" :key="item.data.houseId">
-                <house-item-v9 v-if="item.discoveryContentType === 9" :item-data="item.data" />
-                <house-item-v3 v-else-if="item.discoveryContentType === 3" :item-data="item.data" />
+                <house-item-v9 @click="itemClick(item.data)" v-if="item.discoveryContentType === 9" :item-data="item.data" />
+                <house-item-v3 @click="itemClick(item.data)" v-else-if="item.discoveryContentType === 3" :item-data="item.data" />
             </template>
         </div>
     </div>
@@ -15,10 +15,16 @@ import HouseItemV9 from "@/components/house-item-v9/house-item-v9.vue"
 import HouseItemV3 from "@/components/house-item-v3/house-item-v3.vue"
 import useHomeStore from '@/stores/modules/home';
 import { storeToRefs } from 'pinia';
+import { useRouter } from "vue-router";
 
 const homeStore = useHomeStore()
 const { houselist } = storeToRefs(homeStore)
 
+// 点击item跳转到详情页
+const router = useRouter()
+const itemClick = (item) => {
+    router.push("/detail/" + item.houseId)
+}
 </script>
 
 <style lang="less" scoped>
