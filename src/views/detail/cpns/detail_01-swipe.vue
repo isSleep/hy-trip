d639f8dcfc49d8475caebc709733a4712d4fa900
abBug <13782434+abbug@user.noreply.gitee.com>
2024-07-11 18:17:15 +0800

    数据转换处理

--- src/views/detail/cpns/detail_01-swipe.vue
+++ src/views/detail/cpns/detail_01-swipe.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="swipe">
-    <van-swipe class="swipe-list" :autoplay="3000" indicator-color="white">
-        <template v-for="(item, index) in swipeData">
-            <van-swipe-item class="item">
-                <img :src="item.url" alt="">
-            </van-swipe-item>
-        </template>
-    </van-swipe>
-  </div>
+    <div class="swipe">
+        <van-swipe class="swipe-list" :autoplay="3000" indicator-color="white">
+            <template v-for="(item, index) in swipeData">
+                <van-swipe-item class="item">
+                    <img :src="item.url" alt="">
+                </van-swipe-item>
+            </template>
+
+            <!-- 具名插槽 -->
+            <template #indicator="{ active, total }">
+                <!-- <div class="indicator">{{ active }}/{{ swipeData.length }}-{{ total }}</div> -->
+                <div class="indicator">
+                    <template v-for="(value, key, index) in swipeGroup" :key="key">
+                        <span class="item" :class="{ active: swipeData[active]?.enumPictureCategory == key }">
+                            <span class="text">{{ getName(value[0].title) }}</span>
+                        </span>
+                    </template>
+                </div>
+            </template>
+        </van-swipe>
+    </div>
 </template>
 
 <script setup>
@@ -18,6 +30,36 @@ const props = defineProps({
         default: () => []
     }
 })
+
+//对数据进行转换
+const swipeGroup = {}
+
+// 思路2 :一次循环
+for (const item of props.swipeData) {
+    let valueArray = swipeGroup[item.enumPictureCategory]
+    if (!valueArray) {
+        valueArray = []
+        swipeGroup[item.enumPictureCategory] = valueArray
+    }
+    valueArray.push(item)
+}
+
+// 思路1: 好理解, 两次循环
+// for (const item of props.swipeData) {
+//   swipeGroup[item.enumPictureCategory] = []
+// }
+// for (const item of props.swipeData) {
+//   const valueArray = swipeGroup[item.enumPictureCategory]
+//   valueArray.push(item)
+// }
+
+// 去除数据的【】和:
+const nameReg = /【(.*?)】/i
+const getName = (name) => {
+    // return name.replace(":","").replace("【","").replace("】","")
+    const results = nameReg.exec(name)
+    return results[1]
+}
 </script>
 
 <style lang="less" scoped>
@@ -27,7 +69,29 @@ const props = defineProps({
             img {
                 width: 100%;
             }
-            
+
+        }
+
+        .indicator {
+            position: absolute;
+            right: 5px;
+            bottom: 5px;
+            display: flex;
+            padding: 2px 5px;
+            font-size: 12px;
+            color: #fff;
+            background: rgba(0, 0, 0, 0.8);
+
+            .item {
+                margin: 0 3px;
+
+                &.active {
+                    padding: 0 3px;
+                    border-radius: 5px;
+                    background-color: #fff;
+                    color: #333;
+                }
+            }
         }
     }
 }
