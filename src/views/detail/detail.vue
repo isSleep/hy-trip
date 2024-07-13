bb8b8f01059f428c0f29d73224c287c8e94303bc
abBug <13782434+abbug@user.noreply.gitee.com>
2024-07-13 15:02:52 +0800

    详情-房东、评论、须知页面搭载

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -11,6 +11,9 @@
     <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
     <detail-infos :top-infos="mainPart.topModule" />
     <detail-facility name="设施"  :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"/>
+    <detail-landlord name="房东"  :landlord="mainPart.dynamicModule.landlordModule"/>
+      <detail-comment name="评论"  :comment="mainPart.dynamicModule.commentModule"/>
+      <detail-notice name="须知"  :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
 
   </div>
 </template>
@@ -22,6 +25,9 @@ import { getDetailInfos } from '@/services'
 import DetailSwipe from "./cpns/detail_01-swipe.vue"
 import DetailInfos from "./cpns/detail_02-infos.vue"
 import DetailFacility from "./cpns/detail_03-facility.vue"
+import DetailLandlord from "./cpns/detail_04-landlord.vue"
+import DetailComment from "./cpns/detail_05-comment.vue"
+import DetailNotice from "./cpns/detail_06-notice.vue"
 const router = useRouter()
 const route = useRoute()
 const houseId = route.params.id
