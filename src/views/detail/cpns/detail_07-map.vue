58f5c1d3633ae385eb75db68c1eeabc3b9652904
abBug <13782434+abbug@user.noreply.gitee.com>
2024-07-14 18:47:37 +0800

    详情-地图显示(未完善)

---
+++ src/views/detail/cpns/detail_07-map.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <detail-section title="位置周边" more-text="查看更多">
+        <div class="baidu" ref="mapRef"></div>
+    </detail-section>
+  </div>
+</template>
+
+<script setup>
+import detailSection from '@/components/detail-section/detail-section.vue';
+import { onMounted, ref, defineProps } from 'vue';
+
+const props = defineProps({
+    position: {
+        type: Object,
+        default: () => ({})
+    }
+})
+
+onMounted(() => {
+    const map = new BMapGL.Map(mapRef.value); // 创建地图实例 
+    const point = new BMapGL.Point(props.position.longitude, props.position.latitude); // 创建点坐标 
+    map.centerAndZoom(point, 15); // 初始化地图，设置中心点坐标和地图级别
+    const marker = new BMapGL.Marker(point);  
+    map.addOverlay(marker)
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="detail">
-    <van-nav-bar
-      title="房屋详情"
-      left-text="旅途"
-      left-arrow
-      @click-left="onClickLeft"
-    />
+    <van-nav-bar title="房屋详情" left-text="旅途" left-arrow @click-left="onClickLeft" />
   </div>
   <div class="main" v-if="mainPart">
-    <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
+    <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics" />
     <detail-infos :top-infos="mainPart.topModule" />
-    <detail-facility name="设施"  :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"/>
-    <detail-landlord name="房东"  :landlord="mainPart.dynamicModule.landlordModule"/>
-      <detail-comment name="评论"  :comment="mainPart.dynamicModule.commentModule"/>
-      <detail-notice name="须知"  :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
+    <detail-facility name="设施" :house-facility="mainPart.dynamicModule.facilityModule.houseFacility" />
+    <detail-landlord name="房东" :landlord="mainPart.dynamicModule.landlordModule" />
+    <detail-comment name="评论" :comment="mainPart.dynamicModule.commentModule" />
+    <detail-notice name="须知" :order-rules="mainPart.dynamicModule.rulesModule.orderRules" />
+    <!-- <detail-map name="周边" :position="mainPart.dynamicModule.positionModule" /> -->
+
 
   </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
-import { useRoute,useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { getDetailInfos } from '@/services'
 import DetailSwipe from "./cpns/detail_01-swipe.vue"
 import DetailInfos from "./cpns/detail_02-infos.vue"
@@ -28,6 +25,8 @@ import DetailFacility from "./cpns/detail_03-facility.vue"
 import DetailLandlord from "./cpns/detail_04-landlord.vue"
 import DetailComment from "./cpns/detail_05-comment.vue"
 import DetailNotice from "./cpns/detail_06-notice.vue"
+// import DetailMap from "./cpns/detail_07-map.vue"
+
 const router = useRouter()
 const route = useRoute()
 const houseId = route.params.id
@@ -36,17 +35,15 @@ const houseId = route.params.id
 const detailInfos = ref({})
 const mainPart = computed(() => detailInfos.value.mainPart)
 getDetailInfos(houseId).then(res => {
-    detailInfos.value = res.data
+  detailInfos.value = res.data
 })
 
 // 监听返回按钮的点击
 const onClickLeft = () => {
-    router.back()
+  router.back()
 }
 
 
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
